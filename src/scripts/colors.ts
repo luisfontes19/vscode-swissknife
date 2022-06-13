c7468166a90df712470072bd1ad306df7ef6b17a
Luis Fontes <luisfontes@protonmail.com>
2022-06-13 12:42:06 +0100

    Change the color script to search the entire text for colors and convert
    closes #10

--- src/scripts/colors.ts
+++ src/scripts/colors.ts
@@ -2,41 +2,57 @@ import { IScript, ISwissKnifeContext } from "../Interfaces"
 import { leftPad } from './utils'
 
 export const rgbToHex = async (text: string): Promise<string> => {
-  const rgbArray = text.includes(",") ? text.split(",") : text.split(" ")
+  const reg1 = /rgba?\(\s?\d{1,3}\s?,\s?\d{1,3}\s?,\s?\d{1,3},?\s?\d{0,3}\)/g
+  const reg2 = /\s?\d{1,3}\s?,\s?\d{1,3}\s?,\s?\d{1,3}\s?,?\s?\d{0,3}/
 
-  if (rgbArray.length < 3 || rgbArray.length > 4) throw (new Error("Invalid RGB format"))
-
-  try {
-    return "#" + rgbArray.map((c, i) => {
+  const convertToHex = (str: string) => {
+    const colorArray = str.split(",")
+    return "#" + colorArray.map((c, i) => {
       let n = parseInt(c)
       if (i === 3) n = n * 255 // rgba
       return leftPad(n.toString(16), 2)
     }).join("").toUpperCase()
   }
-  catch (error) {
-    throw (new Error("Invalid RGB value"))
-  }
 
+  try {
+
+    // if the entire text is just an RGB(A) color, like 255,255,255 just convert it
+    if (text.match(new RegExp(`^${reg2.source}$`))) return convertToHex(text)
+
+    // if the text has more then just a color, search for something like rgb(255,255,255) and replace it
+    text.match(reg1)?.forEach(m => {
+      const v = m.match(reg2)![0]
+      text = text.replace(m, convertToHex(v))
+    })
+  }
+  catch (error) { }
+
+  return text
 }
 
 
 export const hexToRgb = async (text: string): Promise<string> => {
-  const match = text.toUpperCase().match(/^#?[0-9A-F]{6}$|^#?[0-9A-F]{8}$/)
-  if (!match) throw (new Error("Invalid Hex color"))
+  const matches = text.match(/#?[0-9A-F]{8}|#?[0-9A-F]{6}/ig)
 
-  text = text.replace("#", "")
+  const convertToHex = (hex: string) => {
 
-  let rgb: Array<number> = []
-  for (let i = 0; i < 3; i++) {
-    const n = text.substring(i * 2, (i * 2) + 2)
-    rgb.push(parseInt(n, 16))
+    hex = hex.replace("#", "")
+
+    let rgb: Array<number> = []
+    for (let i = 0; i < hex.length / 2; i++) {
+      const n = hex.substring(i * 2, (i * 2) + 2)
+      rgb.push(parseInt(n, 16))
+    }
+
+    // rgba
+    const func = hex.length === 8 ? "rgba" : "rgb"
+
+    return `${func}(${rgb.join(",")})`
   }
 
-  // rgba
-  if (text.length === 8) rgb.push(parseInt(text.substring(5, 7), 16) / 255)
+  matches?.forEach(m => text = text.replace(m, convertToHex(m)))
 
-  return rgb.join(",")
-
+  return text
 }
 
 
